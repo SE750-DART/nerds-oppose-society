e701f38e70dad786a8e69b6f97f009cda2963dd4
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-04-27 04:27:36 +1200

    Add Prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,18 @@
 import dotenv from "dotenv";
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import http from "http";
-import {Server as IOServer, Socket} from "socket.io";
+import { Server as IOServer, Socket } from "socket.io";
 
 dotenv.config();
 
-
 // Setup Environment Variables
 if (!process.env.PORT) {
-    console.error("ERROR: Environment variable \"PORT\" not set");
-    process.exit(1);
+  console.error('ERROR: Environment variable "PORT" not set');
+  process.exit(1);
 }
 const PORT: number = parseInt(process.env.PORT as string, 10);
-
 
 // Setup Express
 const app = express();
@@ -23,32 +21,30 @@ app.use(helmet());
 app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
-    try {
-        await new Promise(resolve => setTimeout(resolve, 100));
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
-        res.status(200).send("Hello world!");
-    } catch (e) {
-        res.status(500).send("Something went wrong :/");
-    }
+    res.status(200).send("Hello world!");
+  } catch (e) {
+    res.status(500).send("Something went wrong :/");
+  }
 });
-
 
 // Activate Server
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
-
 
 // Setup Socket.IO
 const io = new IOServer(server, {
-    cors: {
-        origin: true
-    }
+  cors: {
+    origin: true,
+  },
 });
 
 io.on("connection", async (socket: Socket) => {
-    console.log("client connected!");
-    socket.emit("hello", "world!");
-})
+  console.log("client connected!");
+  socket.emit("hello", "world!");
+});
