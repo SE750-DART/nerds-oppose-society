8626718bad0685add6a6d81b69f06adc6b2607c5
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-04-27 01:10:33 +1200

    Express Hello World

---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import express, {Request, Response} from "express";
+import cors from "cors";
+import helmet from "helmet";
+import http from "http";
+
+dotenv.config();
+
+
+// Setup Environment Variables
+if (!process.env.PORT) {
+    console.error("ERROR: Environment variable \"PORT\" not set");
+    process.exit(1);
+}
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+
+// Setup Express
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+    try {
+        await new Promise(resolve => setTimeout(resolve, 100));
+
+        res.status(200).send("Hello world!");
+    } catch (e) {
+        res.status(500).send("Something went wrong :/");
+    }
+});
+
+
+// Activate Server
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
