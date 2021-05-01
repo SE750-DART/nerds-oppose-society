a777d7eb55bbe1007e6adf2c2009132ae7503bc7
Di Kun Ong <dikuno10two@gmail.com>
2021-05-01 12:48:41 +1200

    Add prop for TextField size

--- frontend/src/components/TextField/index.tsx
+++ frontend/src/components/TextField/index.tsx
@@ -4,18 +4,33 @@ import styles from "./style.module.css";
 interface TextFieldProps {
   label: string;
   textValue: string;
+  size?: "big" | "small";
   // eslint-disable-next-line no-unused-vars
   onChangeHandler: (arg0: string) => void;
 }
 
-const TextField = ({ label, textValue, onChangeHandler }: TextFieldProps) => (
-  <input
-    className={styles.textfield}
-    placeholder={label}
-    value={textValue}
-    type="text"
-    onChange={(e) => onChangeHandler(e.target.value)}
-  />
-);
+const TextField = ({
+  label,
+  textValue,
+  size = "big",
+  onChangeHandler,
+}: TextFieldProps) => {
+  let sizeStyle: string = "";
+  if (size === "big") {
+    sizeStyle = styles.textfield__big;
+  } else if (size === "small") {
+    sizeStyle = styles.textfield__small;
+  }
+
+  return (
+    <input
+      className={`${styles.textfield} ${sizeStyle}`}
+      placeholder={label}
+      value={textValue}
+      type="text"
+      onChange={(e) => onChangeHandler(e.target.value)}
+    />
+  );
+};
 
 export default TextField;
