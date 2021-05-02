f3b89ccc2bc0050852c4b8683caa5008deac7009
Di Kun Ong <dikuno10two@gmail.com>
2021-05-02 12:01:02 +1200

    Resolve App page merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
-import React, { createContext } from "react";
+import React, { useState, createContext } from "react";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import HomePage from "./HomePage";
 import GameRouter from "./GameRouter";
+import Button from "./components/Button";
+import TextField from "./components/TextField";
+import PlayerList from "./components/PlayerList";
 import "./App.css";
 
 const browserHistory = createBrowserHistory();
 export const BrowserHistoryContext = createContext(browserHistory);
 
 function App() {
+  const [testText, setTestText] = useState("");
+
+  const clickBtn = () => {
+    console.log("Beep, boop");
+  };
+
+  const logTextField = () => {
+    console.log(testText);
+  };
+
   return (
     <Router history={browserHistory}>
       <Switch>
@@ -28,6 +41,43 @@ function App() {
       <p data-testid="test" style={{ display: "none" }}>
         test
       </p>
+
+      <div className="main">
+        <TextField
+          label="Game code"
+          textValue={testText}
+          onChangeHandler={setTestText}
+        />
+        <br />
+        <Button
+          variant="primary"
+          text="Join game"
+          handleOnClick={logTextField}
+        />
+        <br />
+        <Button
+          variant="secondary"
+          size="small"
+          text="Copy"
+          handleOnClick={clickBtn}
+        />
+        <br />
+        <Button
+          variant="secondary"
+          text="Start new game"
+          handleOnClick={clickBtn}
+        />
+        <br />
+        <TextField
+          label="Score"
+          textValue={testText}
+          size="small"
+          onChangeHandler={setTestText}
+          disabled
+        />
+        <br />
+        <PlayerList gameState="lobby" />
+      </div>
     </Router>
   );
 }
