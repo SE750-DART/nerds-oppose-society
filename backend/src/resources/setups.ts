6bd7b4d0329d29ca28e7ae8091b11839349fb5d2
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-05-02 22:39:03 +1200

    Add writings from Cards Against Humanity

---
+++ backend/src/resources/setups.ts
@@ -0,0 +1,303 @@
+import { Setup, SetupType } from "../models";
+
+/**
+ * These writings are attributed to Cards Against Humanity, LLC. and are remixed
+ * in accordance with the Creative Commons BY-NC-SA 4.0 license.
+ */
+export default <Setup[]>[
+  {
+    setup: "Hey Reddit! I'm __________________. Ask me anything.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Introducing X-treme Baseball! It's like baseball, but with __________!",
+    type: SetupType.pickOne,
+  },
+  { setup: "What is Batman's guilty pleasure.", type: SetupType.pickOne },
+  {
+    setup: "TSA guidelines now prohibit __________________ on airplanes.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Next from J.K. Rowling: Harry Potter and the Chamber of __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "That's right, I killed _________________. How, you ask? _________________.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup:
+      "I'm sorry, Professor, but I couldn't complete my homework because of __________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "And the Academy Award for __________________ goes to __________.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "Dude, do not go in that bathroom. There's __________ in there.",
+    type: SetupType.pickOne,
+  },
+  { setup: "How did I lose my virginity?", type: SetupType.pickOne },
+  {
+    setup:
+      "It's a pity that kids these days are all getting involved with _____________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "Step 1: __________. Step 2: __________. Step 3: Profit.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "_________________. Betcha can't have just one!",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "Kids, I don't need drugs to get high. I'm high on __________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "For my next trick, I will pull __________ out of ___________.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup:
+      "While the United States raced the Soviet Union to the moon, the Mexican government funneled millions of pesos into research on _____________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "In the Disney Channel Original Movie, Hannah Montana struggles with _____________ for the first time.",
+    type: SetupType.pickOne,
+  },
+  { setup: "What's my secret power?", type: SetupType.pickOne },
+  {
+    setup:
+      "I'm going on a cleanse this week. Nothing but kale juice and __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "________ + ________ = ________________.",
+    type: SetupType.drawTwoPickThree,
+  },
+  {
+    setup:
+      "When Pharaoh remained unmoved, Moses called down a Plague of __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      'Just once, I\'d like to hear you say "Thanks, Mom. Thanks for _________________."',
+    type: SetupType.pickOne,
+  },
+  { setup: "Daddy, why is mommy crying?", type: SetupType.pickOne },
+  {
+    setup:
+      "When I was tripping on acid, __________________ turned into _________________.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "50% of all marriages end in __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "My fellow Americans: Before this decade is out, we will have __________________ on the moon!",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "This season at Steppenwolf, Samuel Beckett's classic existential play: Waiting for __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Instead of coal, Santa now gives the bad children __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Life for American Indians was forever changed when the White Man introduced them to __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "What's Teach For America using to inspire inner city students to succeed?",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "Maybe she's born with it. Maybe it's __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "What is George W. Bush thinking about right now?",
+    type: SetupType.pickOne,
+  },
+  { setup: "White people like __________________.", type: SetupType.pickOne },
+  {
+    setup:
+      "__________________ is a slippery slope that leads to _________________.",
+    type: SetupType.pickTwo,
+  },
+  { setup: "Why do I hurt all over?", type: SetupType.pickOne },
+  {
+    setup:
+      "A romantic, candlelit dinner would be incomplete without __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "Just saw this upsetting video! Please retweet!! #stop_____________",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Fun tip! When your man asks you to go down on him, try surprising him with __________________ instead.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "The class field trip was completely ruined by __________________.",
+    type: SetupType.pickOne,
+  },
+  { setup: "What's a girl's best friend?", type: SetupType.pickOne },
+  {
+    setup:
+      "Dear Abby, I'm having some trouble with __________________ and would like your advice.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "In M. Night Shyamalan's new movie, Bruce Willis discovers that __________had really been _______________ all along.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup:
+      "When I am President, I will create the Department of __________________.",
+    type: SetupType.pickOne,
+  },
+  { setup: "What are my parents hiding from me?", type: SetupType.pickOne },
+  { setup: "What never fails to liven up the party?", type: SetupType.pickOne },
+  {
+    setup: "IF you like __________________, YOU MIGHT BE A REDNECK.",
+    type: SetupType.pickOne,
+  },
+  { setup: "Make a haiku.", type: SetupType.drawTwoPickThree },
+  { setup: "What made my first kiss so awkward?", type: SetupType.pickOne },
+  {
+    setup:
+      "Hey guys, welcome to Chili's! Would you like to start the night off right with _________________?",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "I got 99 problems but _______________ ain't one.",
+    type: SetupType.pickOne,
+  },
+  { setup: "__________________. It's a trap!", type: SetupType.pickOne },
+  {
+    setup:
+      "Bravo's new reality show features eight washed-up celebrities living with _____________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "What would grandma find disturbing, yet oddly charming?",
+    type: SetupType.pickOne,
+  },
+  { setup: "__________________. That was so metal.", type: SetupType.pickOne },
+  {
+    setup:
+      "I never truly understood __________________ until I encountered _________________.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "During sex, I like to think about __________________.",
+    type: SetupType.pickOne,
+  },
+  { setup: "What ended my last relationship?", type: SetupType.pickOne },
+  { setup: "What's that sound?", type: SetupType.pickOne },
+  {
+    setup:
+      "Uh, hey guys, I know this was my idea, but I'm having serious doubts about ____________.",
+    type: SetupType.pickOne,
+  },
+  { setup: "Why am I sticky?", type: SetupType.pickOne },
+  {
+    setup:
+      "I'm no doctor but I'm pretty sure what you're suffering from is called \"__________.\"",
+    type: SetupType.pickOne,
+  },
+  { setup: "What's there a ton of in heaven?", type: SetupType.pickOne },
+  {
+    setup:
+      "After four platinum albums and three Grammys, it's time to get back to my roots, to what inspired me to make music in the first place: __________.",
+    type: SetupType.pickOne,
+  },
+  { setup: "What will always get you laid?", type: SetupType.pickOne },
+  {
+    setup:
+      "They said we were crazy. They said we couldn't put __________________ inside of _________. They were wrong.",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup:
+      'Lifetime® presents "_________________: the Story of _________________."',
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "__________________: kid-tested, mother-approved.",
+    type: SetupType.pickOne,
+  },
+  { setup: "Why can't I sleep at night?", type: SetupType.pickOne },
+  { setup: "What's that smell?", type: SetupType.pickOne },
+  {
+    setup:
+      "After eight years in the White House, how is Obama finally letting loose?",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "This is the way the world ends This is the way the world ends Not with a bang but with __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup: "Coming to Broadway this season, __________________: The Musical.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Here is the church Here is the steeple Open the doors And there is __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "But before I kill you, Mr. Bond, I must show you __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "A recent laboratory study shows that undergraduates have 50% less sex after being exposed to ___________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Introducing the amazing superhero/sidekick duo! It's __________ and _____________!",
+    type: SetupType.pickTwo,
+  },
+  {
+    setup: "Next on ESPN2: The World Series of __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "When I am a billionaire, I shall erect a 50-foot statue to commemorate __________________.",
+    type: SetupType.pickOne,
+  },
+  {
+    setup:
+      "Military historians remember Alexander the Great for his brilliant use of ____________________ against the Persians.",
+    type: SetupType.pickOne,
+  },
+  { setup: "War! What is it good for?", type: SetupType.pickOne },
+];
