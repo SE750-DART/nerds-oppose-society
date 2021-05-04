691fd45a6a89471db06961adaf00f55ce6ce997c
Rawiri Hohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-05-05 08:53:38 +1200

    Add methods to call REST API from frontend (#46)
    * Create functions to call api endpoints
    
    * Add unit tests for success cases
    
    * Change axios error handling to try/catch
    
    * Add unit tests for 500 server errors
    
    * Add unit tests for 4xx cases
    
    * Extract duplicate code to generic axios call method
    
    * Add base url to api calls, add dummy buttons to call api, use query params in createPlayer
    
    * Remove console logs, display api responses in p element
    
    * Change commented status codes to documentation comments

---
+++ frontend/src/api/__tests__/validateGame.test.ts
@@ -0,0 +1,93 @@
+import mockAxios from "jest-mock-axios";
+import { AxiosError, AxiosResponse } from "axios";
+import validateGame from "../validateGame";
+import { BASE_URL } from "../axiosCall";
+
+const testGameCode = { gameCode: "12345" };
+
+afterEach(() => {
+  mockAxios.reset();
+});
+
+test("should return success on valid game code", async () => {
+  const mockResponse: AxiosResponse<{}> = {
+    data: {},
+    status: 204,
+    statusText: "NO CONTENT",
+    headers: {},
+    config: {},
+  };
+
+  mockAxios.get.mockImplementationOnce(() => Promise.resolve(mockResponse));
+
+  const res = await validateGame(testGameCode);
+
+  expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  expect(mockAxios.get).toHaveBeenCalledWith(`${BASE_URL}/game/validate`, {
+    params: testGameCode,
+  });
+
+  expect(res).toEqual({
+    success: true,
+    status: 204,
+    data: {},
+  });
+});
+
+test("should return error on 404 not found (invalid code)", async () => {
+  const mockError: AxiosError<{}> = {
+    name: "error",
+    message: "test message",
+    config: {},
+    response: {
+      data: {},
+      status: 404,
+      statusText: "NOT FOUND",
+      headers: {},
+      config: {},
+    },
+    isAxiosError: true,
+    toJSON: () => ({}),
+  };
+
+  mockAxios.get.mockImplementationOnce(() => Promise.reject(mockError));
+
+  const res = await validateGame(testGameCode);
+
+  expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  expect(mockAxios.get).toHaveBeenCalledWith(`${BASE_URL}/game/validate`, {
+    params: testGameCode,
+  });
+
+  expect(res).toEqual({
+    success: false,
+    status: 404,
+    error: {},
+  });
+});
+
+test("should return error on 500 server error", async () => {
+  const mockError: AxiosError<{}> = {
+    name: "error",
+    message: "test message",
+    config: {},
+    request: "test request",
+    isAxiosError: true,
+    toJSON: () => ({}),
+  };
+
+  mockAxios.get.mockImplementationOnce(() => Promise.reject(mockError));
+
+  const res = await validateGame(testGameCode);
+
+  expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  expect(mockAxios.get).toHaveBeenCalledWith(`${BASE_URL}/game/validate`, {
+    params: testGameCode,
+  });
+
+  expect(res).toEqual({
+    success: false,
+    status: 500,
+    error: "test request",
+  });
+});
