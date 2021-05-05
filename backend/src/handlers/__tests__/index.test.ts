9654d6ae4ac9a46d7dc17d0120700daacccb5d78
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-05-05 14:11:39 +1200

    Register Game handler

--- backend/src/handlers/__tests__/index.test.ts
+++ backend/src/handlers/__tests__/index.test.ts
@@ -1,18 +1,22 @@
 import * as PlayerHandler from "../player.handler";
+import * as GameHandler from "../game.handler";
 import { Server, Socket } from "socket.io";
 import { Connection } from "../index";
 
 describe("Connection handler", () => {
   let playerSpy: jest.SpyInstance;
+  let gameSpy: jest.SpyInstance;
   let joinSpy: jest.SpyInstance;
 
   beforeEach(() => {
     playerSpy = jest.spyOn(PlayerHandler, "default").mockImplementation();
+    gameSpy = jest.spyOn(GameHandler, "default").mockImplementation();
     joinSpy = jest.spyOn(PlayerHandler, "playerJoin").mockImplementation();
   });
 
   afterEach(() => {
     joinSpy.mockRestore();
+    gameSpy.mockRestore();
     playerSpy.mockRestore();
   });
 
@@ -24,6 +28,9 @@ describe("Connection handler", () => {
 
     expect(playerSpy).toHaveBeenCalledTimes(1);
     expect(playerSpy).toHaveBeenCalledWith("io", "socket");
+
+    expect(gameSpy).toHaveBeenCalledTimes(1);
+    expect(gameSpy).toHaveBeenCalledWith("io", "socket");
 
     expect(joinSpy).toHaveBeenCalledTimes(1);
     expect(joinSpy).toHaveBeenCalledWith(io, socket);
