6143d448b09df89894ddce0ec625537c64a2a8c2
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2022-07-12 23:09:18 +1200

    Fix context hook tests (#135)
    * Fix context hook tests
    
    * Use renderHook() for context tests

--- frontend/src/contexts/__tests__/players.test.tsx
+++ frontend/src/contexts/__tests__/players.test.tsx
@@ -1,31 +1,22 @@
-import React from "react";
+import { renderHook } from "@testing-library/react-hooks";
 import { PlayersProvider, usePlayers } from "../players";
-import { render } from "@testing-library/react";
-import ProviderWrapper from "../../utils/testing/ProviderWrapper";
-
-const PlayersWrapper = () => <ProviderWrapper Provider={PlayersProvider} />;
 
 describe("usePlayers()", () => {
-  const Component = () => {
-    const { players, host } = usePlayers();
+  it("returns players context when used inside PlayerProvider", () => {
+    const { result } = renderHook(() => usePlayers(), {
+      wrapper: PlayersProvider,
+    });
+
+    const { players, host } = result.current;
     expect(players).toEqual([]);
     expect(host).toBe("");
-    return <></>;
-  };
-
-  it("returns players context when used inside PlayerProvider", () => {
-    render(<Component />, { wrapper: PlayersWrapper });
   });
 
   it("throws error when used outside PlayerProvider", () => {
-    /*
-    Note: This throws an error in the test console:
-    Uncaught [Error: usePlayers() must be used within a PlayersProvider]
-    There does not appear to be a way to disable this however the test is caught
-    in the code below - nothing to see here!
-     */
-    expect(() => render(<Component />)).toThrow(
-      "usePlayers() must be used within a PlayersProvider"
+    const { result } = renderHook(() => usePlayers());
+
+    expect(result.error).toEqual(
+      Error("usePlayers() must be used within a PlayersProvider")
     );
   });
 });
